6c62fb4694c10650cdb0c5d20ca6f923474c80d3
Ivan Podogov <ginkage@yandex.ru>
2023-11-06 21:51:07 +0000

    Formatting nits

--- HID-Controller/src/ATMEGA32u4.cpp
+++ HID-Controller/src/ATMEGA32u4.cpp
@@ -477,34 +477,34 @@ void mouseControl() {
 
 static void turnOffPWM(uint8_t timer)
 {
-	switch (timer)
-	{
-		case TIMER1A: cbi(TCCR1A, COM1A1); break;
-		case TIMER1B: cbi(TCCR1A, COM1B1); break;
-		case TIMER1C: cbi(TCCR1A, COM1C1); break;
-		case TIMER0A: cbi(TCCR0A, COM0A1); break;
-		case TIMER0B: cbi(TCCR0A, COM0B1); break;
-		case TIMER3A: cbi(TCCR3A, COM3A1); break;
-		case TIMER3B: cbi(TCCR3A, COM3B1); break;
-		case TIMER3C: cbi(TCCR3A, COM3C1); break;
-		case TIMER4A: cbi(TCCR4A, COM4A1); break;
-		case TIMER4B: cbi(TCCR4A, COM4B1); break;
-		case TIMER4D:	cbi(TCCR4C, COM4D1); break;
-	}
+  switch (timer)
+  {
+    case TIMER1A: cbi(TCCR1A, COM1A1); break;
+    case TIMER1B: cbi(TCCR1A, COM1B1); break;
+    case TIMER1C: cbi(TCCR1A, COM1C1); break;
+    case TIMER0A: cbi(TCCR0A, COM0A1); break;
+    case TIMER0B: cbi(TCCR0A, COM0B1); break;
+    case TIMER3A: cbi(TCCR3A, COM3A1); break;
+    case TIMER3B: cbi(TCCR3A, COM3B1); break;
+    case TIMER3C: cbi(TCCR3A, COM3C1); break;
+    case TIMER4A: cbi(TCCR4A, COM4A1); break;
+    case TIMER4B: cbi(TCCR4A, COM4B1); break;
+    case TIMER4D: cbi(TCCR4C, COM4D1); break;
+  }
 }
 
 int atDigitalRead(uint8_t pin)
 {
-	uint8_t port = pinout[pin].port;
-	uint8_t bit = (1 << pinout[pin].bit);
-	uint8_t timer = pinout[pin].timer;
+  uint8_t port = pinout[pin].port;
+  uint8_t bit = (1 << pinout[pin].bit);
+  uint8_t timer = pinout[pin].timer;
 
-	// If the pin that support PWM output, we need to turn it off
-	// before getting a digital reading.
-	if (timer != NOT_ON_TIMER) turnOffPWM(timer);
+  // If the pin that support PWM output, we need to turn it off
+  // before getting a digital reading.
+  if (timer != NOT_ON_TIMER) turnOffPWM(timer);
 
-	if (*portInputRegister(port) & bit) return HIGH;
-	return LOW;
+  if (*portInputRegister(port) & bit) return HIGH;
+  return LOW;
 }
 
 void buttonRead() { // Read button inputs and set state arrays.
@@ -708,12 +708,12 @@ void joystickInput() {
 
 void atPinModeInputPullup(uint8_t pin)
 {
-	uint8_t port = pinout[pin].port;
-	uint8_t bit = (1 << pinout[pin].bit);
+  uint8_t port = pinout[pin].port;
+  uint8_t bit = (1 << pinout[pin].bit);
 
-	volatile uint8_t *reg, *out;
-	reg = portModeRegister(port);
-	out = portOutputRegister(port);
+  volatile uint8_t *reg, *out;
+  reg = portModeRegister(port);
+  out = portOutputRegister(port);
 
   uint8_t oldSREG = SREG;
   cli();
