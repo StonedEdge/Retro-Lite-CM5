c12f249fb3c21093f66babc34ed6386cc2e84006
Ivan Podogov <ginkage@yandex.ru>
2024-06-02 14:05:28 +0100

    Rename Mali X11 folder, add universal malirun script

--- armbian/userpatches/overlay/malirun_x11.sh
+++ armbian/userpatches/overlay/malirun_x11.sh
@@ -3,7 +3,7 @@ APP=$(which $1)
 ARGS=${@:2}
 HASRPATH=$(chrpath -l $APP | grep -o /usr/lib)
 export LIBGL_DEEPBIND=0
-export LD_LIBRARY_PATH="/usr/lib/gl4es:/usr/lib/libmali/x:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/lib/gl4es:/usr/lib/libmali/x11:$LD_LIBRARY_PATH"
 export LD_PRELOAD="/usr/lib/libdri2to3.so:$LD_PRELOAD"
 # linux linker loads the shared libs in the order of 1. RPATH of the elf, then LD_LIBRARY_PATH
 # if a binary has RPATH pointed out to /usr/lib, then this causes system GL libraries to load
